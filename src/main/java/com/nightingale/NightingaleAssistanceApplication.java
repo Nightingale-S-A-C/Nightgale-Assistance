8f3d40d925fd78b2f6833d128dfe9d8317e8126d
chorri <josemariacosta_8@hotmail.com>
2020-09-27 13:23:24 -0500

    Nuevo proyecto creado

---
+++ src/main/java/com/nightingale/NightingaleAssistanceApplication.java
@@ -0,0 +1,13 @@
+package com.nightingale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightingaleAssistanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NightingaleAssistanceApplication.class, args);
+    }
+
+}
