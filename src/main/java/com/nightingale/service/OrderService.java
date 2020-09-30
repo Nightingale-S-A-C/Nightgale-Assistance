bed4f6ffb1be842ea9c6acfd08863a716ca9b36b
Hernán Razo <ozemblem66@gmail.com>
2020-09-30 14:22:05 -0500

    Inicializados repositorys, controllers, y services

---
+++ src/main/java/com/nightingale/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.nightingale.service;
+
+
+import com.nightingale.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+
+    // funciones
+}
