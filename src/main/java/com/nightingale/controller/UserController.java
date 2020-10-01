4114b22c0d8e9f156117de8ec9285d916a1e8419
chorri <josemariacosta_8@hotmail.com>
2020-09-30 22:23:45 -0500

    Agregado inicio de UserController

--- src/main/java/com/nightingale/controller/UserController.java
+++ src/main/java/com/nightingale/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.nightingale.controller;
 
 
+import com.nightingale.entity.User;
+import com.nightingale.repository.UserRepository;
 import com.nightingale.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService userService;
+
+//    @GetMapping("/Users")
+//    public Page<User> getUser(Pageable pageable){
+//        //return UserRepository.find
+//    }
 }
