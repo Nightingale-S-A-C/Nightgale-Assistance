70377cd1385ea8fce17658da7974808c5941e2a7
Hernán Razo <ozemblem66@gmail.com>
2020-11-19 20:50:59 -0500

    Import sql
    import.sql edited

--- src/main/java/com/nightingale/controller/ProductController.java
+++ src/main/java/com/nightingale/controller/ProductController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("products")
+@RequestMapping("/products")
 public class ProductController {
     @Autowired
     private ProductService productService;
